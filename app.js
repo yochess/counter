9a1278d2865a11150edff2feed91bd6330fdf585
Daham Drake Wang <drakewang@gmail.com>
2016-09-12 11:54:11 -0700

    Setup mailer feature and endpoint.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 // database config
@@ -8,13 +9,23 @@ mongoose.connect('mongodb://localhost/counter');
 
 // model config
 const counterSchema = mongoose.Schema({
-    counter: Number
+  counter: Number
 });
 const Counter = mongoose.model('Counter', counterSchema);
+
+const mailerSchema = mongoose.Schema({
+  email: String,
+  radio: String,
+  time : { type : Date, default: Date.now }
+});
+const Mailer = mongoose.model('Mailer', mailerSchema);
 
 // view config
 app.set('view engine', 'ejs');
 app.set('views', __dirname);
+
+// bodyParser config
+app.use(bodyParser.json())
 
 // static page config
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,14 +34,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.put('/api/getCounter/:id', (req, res) => {
   const id = req.params.id;
 
-  Counter.findOneAndUpdate({id: id}, {$inc: {counter: 1}}, {
-    upsert: true,
-    returnNewDocument: true
-  }, (err, result) => {
-    console.log('result', result);
+  Counter.findOneAndUpdate(
+  {id: id},
+  {$inc: {counter: 1} },
+  {upsert: true, returnNewDocument: true},
+  (err, result) => {
     return res.send(result);
   });
+});
 
+// API: mailer
+app.post('/api/subscribe', (req, res) => {
+  const email = req.body.email;
+  const radio = req.body.radio;
+
+  Mailer.findOne({email}, (err, result) => {
+    if (result) {
+      return result.update({email, radio}, (err, result) => {
+        return res.send('updated!');
+      })
+    }
+    return Mailer.create({email, radio}, (err, result) => {
+      return res.status(201).send('created!');
+    });
+
+  });
 });
 
 // main page
--- public/app.js
+++ public/app.js
@@ -2,14 +2,20 @@
   const app = angular.module('counterApp', []);
 
   app.controller('counterCtrl', ($scope, Counter) => {
-
+    $scope.input = {};
+    $scope.input.radio = "one";
 
     $scope.getCounter = () => {
       Counter.getCounter(1).then(data => {
         $scope.counter = data.counter
       });
-    }
+    };
 
+    $scope.subscribe = () => {
+      Counter.subscribe($scope.input).then(data => {
+        console.log(data);
+      });
+    };
 
     $scope.getCounter();
   });
@@ -18,8 +24,12 @@
     return {
       getCounter: (id) => {
         return $http.put(`/api/getCounter/${id}`).then(res => res.data);
+      },
+
+      subscribe: (input) => {
+        return $http.post('/api/subscribe', input).then(res => res.data);
       }
-    }
+    };
 
   });
 
